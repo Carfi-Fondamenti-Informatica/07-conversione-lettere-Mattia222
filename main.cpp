cd8bbd4577059f3a5111396fa45217fd9ef6503c
Mattia222 <127505321+Mattia222@users.noreply.github.com>
2023-05-16 20:40:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,8 @@ int main(){
   
   char c;
     cin >> c;
-    funzione (c);
-  
-  if ( ((funzione (c) == true)) and ((c>=65) and (c<=90)) ){
-        c=c+32;
-        cout << c << endl;
-    }else if ( ((funzione (c) == true)) and ((c>=97) and (c<=122)) ){
-        c=c-32;
+
+    if (funzione (c) == true){
         cout << c << endl;
     }else {
         cout << "errore" << endl;
