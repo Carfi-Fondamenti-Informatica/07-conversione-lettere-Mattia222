5b298eddb7ff1041c9f5301fb8411bb814509ea7
Mattia222 <127505321+Mattia222@users.noreply.github.com>
2023-05-16 20:39:57 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,13 +1,16 @@
 #include "Libreria.h"
 
-bool funzione (char c){
+#include "Libreria.h"
+
+bool funzione (char &c){
     if ((c>=65) and (c<=90)){
-        //c=c+32;
+        c=c+32;
     return true;
     }else if ((c>=97) and (c<=122)){
-        //c=c-32;
+        c=c-32;
     return true;
     }else{
         return false;
     }
+
 }
